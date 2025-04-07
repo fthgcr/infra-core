2454efe254e87b2897c02036c198b87b68becfe3
Fatih <fatihhgocer>
2025-04-07 20:23:00 +0300

    Init

---
+++ src/test/java/com/infra_core/InfraCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infra_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
